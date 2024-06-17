ea7c34cfa9cdd0a6124a6bb998bf31ea2493b8c7
Bryce Gardner <bgardner7@gmail.com>
2024-06-16 20:33:32 -0600

    Added headful browser. Tested nav to bestbuy.com

--- bbMtgScraper.js
+++ bbMtgScraper.js
@@ -1,7 +1,15 @@
+import puppeteer from 'puppeteer'
+import fs from "fs"
+import ProgressBar from 'progress'
 
+let sales = fs.existsSync(`sales.json`) ? JSON.parse(fs.readFileSync("sales.json", "utf8")) : []
+
+const browser = await puppeteer.launch({headless: false})
 
 const main = async () => {
-    console.log("Hello World!")
+    const page = await browser.newPage()
+    await page.goto('https://bestbuy.com/')
+    await page.setViewport({width: 1440, height: 2560});
 }
 
 main()
