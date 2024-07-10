1d206083b459f2b1e6a10db96c058ccd911a287a
Bryce Gardner <bgardner7@gmail.com>
2024-07-09 19:17:04 -0600

    Created a delay function. Added delay between search and click.

--- bbMtgScraper.js
+++ bbMtgScraper.js
@@ -2,7 +2,9 @@ import puppeteer from 'puppeteer'
 import fs from "fs"
 import ProgressBar from 'progress'
 
-let sales = fs.existsSync(`sales.json`) ? JSON.parse(fs.readFileSync("sales.json", "utf8")) : []
+let searchValue = 'light laptops'
+
+let sales = fs.existsSync(`sales.json`) ? JSON.parse(fs.readFileSync('sales.json', 'utf8')) : []
 
 const browser = await puppeteer.launch({
     headless: false,
@@ -10,13 +12,23 @@ const browser = await puppeteer.launch({
     args: ['--start-maximized']
 })
 
+// Creates a delay in milliseconds
+function delay(amount) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, amount)
+    })
+}
+
 const main = async () => {
     const page = await browser.newPage()
     await page.goto('https://bestbuy.com/')
     await page.setViewport({width: 1440, height: 2560})
 
     // Type into search box
-    await page.type('.search-input', 'light laptops')
+    await page.type('.search-input', searchValue)
+
+    // Delay to allow search to complete
+    await delay(1000)
 
     // Click search button
     await page.click('.header-search-button')
