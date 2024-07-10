5a5a7f42ad4cc482ea1f052164f44c3419b349bc
Bryce Gardner <bgardner7@gmail.com>
2024-07-09 19:11:58 -0600

    Added search input and button click. Changed browser to launch maximized.

--- bbMtgScraper.js
+++ bbMtgScraper.js
@@ -4,12 +4,22 @@ import ProgressBar from 'progress'
 
 let sales = fs.existsSync(`sales.json`) ? JSON.parse(fs.readFileSync("sales.json", "utf8")) : []
 
-const browser = await puppeteer.launch({headless: false})
+const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-maximized']
+})
 
 const main = async () => {
     const page = await browser.newPage()
     await page.goto('https://bestbuy.com/')
-    await page.setViewport({width: 1440, height: 2560});
+    await page.setViewport({width: 1440, height: 2560})
+
+    // Type into search box
+    await page.type('.search-input', 'light laptops')
+
+    // Click search button
+    await page.click('.header-search-button')
 }
 
 main()
