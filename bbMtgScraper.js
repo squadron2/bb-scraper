bad932ec97152fb8e50cc6327d6a4dd91d960d6d
Bryce Gardner <bgardner7@gmail.com>
2024-07-17 13:56:07 -0600

    Changed search box input and created manual event triggers. Changed searchValue to an object.

--- bbMtgScraper.js
+++ bbMtgScraper.js
@@ -2,7 +2,9 @@ import puppeteer from 'puppeteer'
 import fs from "fs"
 import ProgressBar from 'progress'
 
-let searchValue = 'light laptops'
+// let searchValue = 'magic the gathering'
+let searchValue = {bestbuy: 'magic the gathering'}
+let searchDelay = 3000 + searchValue.bestbuy.length * 100
 
 let sales = fs.existsSync(`sales.json`) ? JSON.parse(fs.readFileSync('sales.json', 'utf8')) : []
 
@@ -22,13 +24,22 @@ function delay(amount) {
 const main = async () => {
     const page = await browser.newPage()
     await page.goto('https://bestbuy.com/')
-    await page.setViewport({width: 1440, height: 2560})
+    await page.setViewport({width: 2560, height: 1440})
+
+    // Element must be visible for typing
+    await page.waitForSelector('.search-input', {visible: true, timeout: 5000})
 
     // Type into search box
-    await page.type('.search-input', searchValue)
+    await page.evaluate((value) => {
+        const input = document.querySelector('#gh-search-input');
+        const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+        nativeInputValueSetter.call(input, value);
+        input.dispatchEvent(new Event('input', { bubbles: true }));
+        input.dispatchEvent(new Event('change', { bubbles: true }));
+    }, searchValue.bestbuy);
 
     // Delay to allow search to complete
-    await delay(1000)
+    await delay(searchDelay)
 
     // Click search button
     await page.click('.header-search-button')
